7c27539830a87f09c1d01f1669e07d398291204c
Francisco Egea <puertorey@gmail.com>
2020-06-23 12:50:34 +0100

    Fixed console warning with tachyons

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './Containers/App';
 import * as serviceWorker from './serviceWorker';
-import tachyons from 'tachyons';
+import 'tachyons';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
