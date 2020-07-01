df9fdb132e90080e46eb1644539a0b4b1d0f0c0c
Francisco Egea <puertorey@gmail.com>
2020-07-01 12:18:54 +0100

    Added the request robots function with redux instead of state, using the store in App.js

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -3,51 +3,41 @@ import './App.css';
 import CardList from '../Components/CardList';
 import SearchBox from '../Components/SearchBox';
 import ScrollComponent from '../Components/ScrollComponent';
-import { setSearchField } from '../actions.js';
+import { setSearchField, requestRobots } from '../actions.js';
 import { connect } from 'react-redux';
 
 const mapStateToProps = state => {
 	return {
-		searchField: state.searchField
+		searchField: state.searchRobots.searchField,
+		robots: state.requestRobots.robots,
+		isPending: state.requestRobots.isPending,
+		error: state.requestRobots.error
 	}
 }
 
 const mapDispatchToProps = (dispatch) => {
 	return {
-		onInputChange: (event) => dispatch(setSearchField(event.target.value))
+		onInputChange: (event) => dispatch(setSearchField(event.target.value)),
+		onRequestRobots: () => dispatch(requestRobots())
 	}
 }
 
 class App extends Component {
-	constructor() {
-		super();
-		this.state = {
-			robots: []
-			//searchfield: ''
-		}
-	}
 
 	componentDidMount(){
-		fetch('https://jsonplaceholder.typicode.com/users')
-			.then(response => response.json())
-			.then(users => {
-				this.setState({ robots: users });
-			});
+		this.props.onRequestRobots()
 	}
-	/**
-	onInputChange = (event) => {
-		this.setState({searchfield: event.target.value})
-	}
-	**/
+	
 	render() {
-		const {/**searchfield,**/ robots} = this.state;
-		const { searchField, onInputChange } = this.props;
+		const { searchField, onInputChange, robots, isPending } = this.props;
 
 		let filteredRobots = robots.filter(robot => {
 			return robot.name.toLowerCase().includes(searchField.toLowerCase())
 		})
 
-		return (
+		return isPending ?
+			<h1> Loading </h1> :
+		(
 	    <div className="App tc">
 	      <h1 className='tc'>Robofriends</h1>
 	      <SearchBox onInputChange={onInputChange}/>
--- src/actions.js
+++ src/actions.js
@@ -11,7 +11,7 @@ export const setSearchField = (text) => ({
 	payload: text
 })
 
-export const requestRobots = (dispatch) => {
+export const requestRobots = () => (dispatch) => {
 	dispatch({ type: REQUEST_ROBOTS_PENDING });
 	fetch('https://jsonplaceholder.typicode.com/users')
 		.then(response => response.json())
